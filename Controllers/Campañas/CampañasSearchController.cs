614fb5a784118a273068606d429e733eb152fc4f
AnthonyCarmine <anthonymunoz302755@correo.itm.edu.co>
2024-05-26 20:48:37 -0500

    TASK-1:Se crea rama desarrollo

---
+++ Controllers/Campañas/CampañasSearchController.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Cupones.Models;
+using Cupones.Services;
+
+namespace Cupones.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CampañasSearchController : ControllerBase
+    {
+        public readonly ICampañasService _campañasService;
+
+        public CampañasSearchController(ICampañasService campañasService)
+        {
+            _campañasService = campañasService;
+        }
+
+    }
+}
