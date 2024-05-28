12922b27f6fce144f91985861917b4480ea3ca89
AnthonyCarmine <anthonymunoz302755@correo.itm.edu.co>
2024-05-27 20:56:17 -0500

    Se actualiza controlador empresas

--- Controllers/Empresa/companiesUpdateController.cs
+++ Controllers/Empresa/companiesUpdateController.cs
@@ -20,11 +20,43 @@ namespace Cupones.Controllers
       _empresasService = empresasService;
     }
 
- /*    [HttpPut("{id}")]
-    [Route("api/empresas/{id}/update")]
-    public string Update(int id, [FromBody] Empresa empresa)
-    {
-      return "Falta actualizar";
-    } */
+   [HttpPut("{id}")]
+        public IActionResult Update(int id, [FromBody] Cupon cupon)
+        {
+            try
+            {
+                // Verifica si la campaña recibida es nula
+                if (cupon == null)
+                {
+                    // Devuelve un BadRequest si la campaña es nula
+                    return BadRequest("Campaña data is null");
+                }
+
+                // Verifica si el ID de la campaña coincide con el ID proporcionado en la ruta
+                if (id != cupon.Id)
+                {
+                    // Devuelve un BadRequest si el ID de la campaña no coincide con el ID en la ruta
+                    return BadRequest("ID mismatch between route parameter and cupon data");
+                }
+
+                var existingCampaign = _cuponesService.GetById(id);
+                if (existingCampaign == null)
+                {
+                    // Devuelve un NotFound si la campaña no existe
+                    return NotFound($"Campaign with Id = {id} not found");
+                }
+
+                // Actualiza la campaña
+                _cuponesService.update(cupon);
+
+                // Devuelve un resultado Ok con un mensaje indicando que la campaña se ha actualizado correctamente
+                return Ok("Campaign updated successfully");
+            }
+            catch (Exception ex)
+            {
+                // Devuelve un estado de error interno del servidor (500) con un mensaje descriptivo
+                return StatusCode(500, $"Error updating campaign: {ex.Message}");
+            }
+        }
   }
 }
