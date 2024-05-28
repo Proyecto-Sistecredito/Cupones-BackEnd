78bdf70bb056a2c0ceb19cbbd3c2a0cac8ebd004
AnthonyCarmine <anthonymunoz302755@correo.itm.edu.co>
2024-05-28 09:30:03 -0500

    Se arregla base de datos

--- db.sql
+++ db.sql
@@ -38,12 +38,14 @@ CREATE TABLE Cupones (
     IdUsuario INT NOT NULL,
     IdTipoUso INT NOT NULL,
     IdCampaña INT NOT NULL,
+    IdActivo INT NOT NULL,
     FOREIGN KEY (IdUsuarioMarketing) REFERENCES UsuariosMarketing(Id),
     FOREIGN KEY (IdTipoCupon) REFERENCES TiposCupones(Id),
     FOREIGN KEY (IdRedimido) REFERENCES Redimido(Id),
     FOREIGN KEY (IdUsuario) REFERENCES Usuarios(Id),
     FOREIGN KEY (IdTipoUso) REFERENCES TiposUsos(Id),
     FOREIGN KEY (IdCampaña) REFERENCES Campañas(Id)
+    FOREIGN KEY (IdActivo) REFERENCES Activo(Id)
 );
 
 -- Tabla: Empresas
@@ -72,6 +74,11 @@ CREATE TABLE Roles (
 -- Datos en la tabla Roles
 INSERT INTO Roles (Tipo) VALUES ('Administrador'), ('UsuarioMarketing');
 
+CREATE TABLE Activo (
+    Id INT AUTO_INCREMENT PRIMARY KEY,
+    Estado VARCHAR(5) NOT NULL
+);
+INSERT INTO Activo (Estado) VALUES ('si'), ('no');
 -- Tabla: TiposCupones
 CREATE TABLE TiposCupones (
     Id INT AUTO_INCREMENT PRIMARY KEY,
@@ -131,3 +138,27 @@ VALUES
 ('Juanky', 'Apellido2', 'juanky@example.com', 'contraseña2',1241245, 1,1),
 ('Maicol', 'Apellido3', 'maicol@example.com', 'contraseña3', 1231245, 1,1),
 ('Juanda', 'Apellido4', 'juanda@example.com', 'contraseña4', 12315123, 1,1);
+
+-- Insertar nuevas empresas
+INSERT INTO Empresas (Nombre, Logo, Nit) VALUES
+('Luego Pago', 'logo_luego_pago.png', 'NIT123456'),
+('Éxito', 'logo_puede_ser_exito.png', 'NIT789012');
+
+-- Insertar nuevas campañas
+-- Campañas para "Luego Pago"
+INSERT INTO Campañas (Nombre, Descripcion, FechaInicio, FechaFin, IdEmpresa) VALUES
+('Descuento Verano', 'Ofertas especiales para el verano', '2024-06-01', '2024-08-31', 1),
+('Semana de Descuentos', 'Grandes descuentos durante una semana', '2024-09-15', '2024-09-22', 1);
+
+-- Campañas para "Éxito"
+INSERT INTO Campañas (Nombre, Descripcion, FechaInicio, FechaFin, IdEmpresa) VALUES
+('Promoción Aniversario', 'Celebramos nuestro aniversario con descuentos exclusivos', '2024-07-15', '2024-07-31', 2),
+('Black Friday', 'Grandes descuentos en productos seleccionados', '2024-11-28', '2024-11-30', 2);
+
+-- Insertar un nuevo usuario
+INSERT INTO Usuarios (Nombre, Apellido, Email, Documento, TipoDocumento)
+VALUES ('Juan', 'Pérez', 'juan@example.com', '123456789', 'cedula');
+
+-- Insertar una nueva compra
+INSERT INTO Compras (ValorCompra, FechaCompra, IdUsuario, IdCupon)
+VALUES (100.00, '2024-05-28 15:30:00', 1, 1);
